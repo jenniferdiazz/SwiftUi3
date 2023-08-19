1a4acf9fac601558f2d579e5e96fdd994da3aaf8
jennifer <jenniferdiaz@outlook.cl>
2023-08-19 12:48:29 -0400

    Initial Commit

---
+++ swiftBeta3/swiftBeta3App.swift
@@ -0,0 +1,17 @@
+//
+//  swiftBeta3App.swift
+//  swiftBeta3
+//
+//  Created by Jennifer Diaz on 19-08-23.
+//
+
+import SwiftUI
+
+@main
+struct swiftBeta3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
